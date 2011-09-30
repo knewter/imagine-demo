93e09257216c4c1124b1baaa7381f75af9f2997f
Josh Adams <josh@isotope11.com>
2011-09-30 16:36:23 -0500

    Don't use dragonfly heroku bits if no env var...

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,6 +1,8 @@
-app_images = Dragonfly[:images] # Grab the rails configured dragonfly app, add heroku s3 support
-app_images.configure_with(:heroku, ENV['S3_BUCKET'])
-# Dragonfly doesn't set the S3 region, so we have to do this manually
-app_images.datastore.configure do |d|
-  d.region = ENV['S3_REGION'] if ENV['S3_REGION'] # otherwise defaults to 'us-east-1'
+if ENV['S3_BUCKET'] # don't try this if that env var is missing, for instance in heroku slug
+  app_images = Dragonfly[:images] # Grab the rails configured dragonfly app, add heroku s3 support
+  app_images.configure_with(:heroku, ENV['S3_BUCKET'])
+  # Dragonfly doesn't set the S3 region, so we have to do this manually
+  app_images.datastore.configure do |d|
+    d.region = ENV['S3_REGION'] if ENV['S3_REGION'] # otherwise defaults to 'us-east-1'
+  end
 end
